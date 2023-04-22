66054de1fe6875ef8f1bd0877ecd1126a2519572
ashwani2908 <76448715+ashwani2908@users.noreply.github.com>
2023-04-22 11:47:55 +0530

    Added Component as ExpenceItem
    Added ExpenceItem in the component folder and created a function to be used in App.js file.
    Added Many other Expense Also.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ExpenceItem from './component/ExpenceItem';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hey I am World </h1>
+      <ExpenceItem></ExpenceItem>
+    </div>
+  );
+}
+
+export default App;
+
+
+
