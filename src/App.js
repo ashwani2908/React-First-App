914ac88b4534f33f3156e564ba44bb2df9e07270
ashwani2908 <76448715+ashwani2908@users.noreply.github.com>
2023-04-23 11:52:23 +0530

    Used props argument and added Location data
    Used prop to display expense data and loop through the expenses and display on ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
-import ExpenceItem from './component/ExpenceItem';
-import './App.css';
+import ExpenseItem from "./component/ExpenceItem";
+import "./App.css";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      locationOfExpenditure: "Home",
+    },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      locationOfExpenditure: "Home",
+    },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      locationOfExpenditure: "Company",
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 7, 12),
+      locationOfExpenditure: "Study",
+    },
+  ];
+
   return (
     <div className="App">
-      <h1> Hey I am World </h1>
-      <ExpenceItem></ExpenceItem>
+      <h1> Expence Tracker </h1>
+      {expenses.map((detail) => {
+        return (
+          <ExpenseItem
+            title={detail.title}
+            amount={detail.amount}
+            date={detail.date}
+            locationOfExpenditure = {detail.locationOfExpenditure}
+          ></ExpenseItem>
+        );
+      })}
     </div>
   );
 }
 
 export default App;
-
-
-
--- src/component/ExpenceItem.js
+++ src/component/ExpenceItem.js
@@ -1,12 +1,17 @@
-function ExpenceItem() {
+import './ExpenceItem.css';
+
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <h2>Expences Item!</h2>
-      <h2>Food Rs 10</h2>
-      <h2>Petrol Rs 100!</h2>
-      <h2>Movies Rs 200</h2>
+    <div className='expense-item'>
+      <div>{props.date.toISOString()}</div>
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <h4>{props.locationOfExpenditure}</h4>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
     </div>
   );
 }
 
-export default ExpenceItem;
+export default ExpenseItem;
