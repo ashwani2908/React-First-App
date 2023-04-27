2278245cd65039c92af6dc316af95c42d479b9a7
ashwani2908 <76448715+ashwani2908@users.noreply.github.com>
2023-04-27 18:50:37 +0530

    Created a new component Expeses

--- src/App.js
+++ src/App.js
@@ -1,53 +1,35 @@
-import ExpenseItem from "./component/Expenses/ExpenceItem";
-import Card from "./component/UI/Card";
-import "./App.css";
+import Expenses from './Components/Expenses';
+import './Components/ExpenseItem.css'
 
-const App = () => {
+function App() {
   const expenses = [
     {
-      id: "e1",
-      title: "Toilet Paper",
+      id: 'e1',
+      title: 'Toilet Paper',
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      locationOfExpenditure: "Home",
     },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      locationOfExpenditure: "Home",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
+      id: 'e3',
+      title: 'Car Insurance',
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      locationOfExpenditure: "Company",
-    },
+    }, 
     {
-      id: "e4",
-      title: "New Desk (Wooden)",
+      id: 'e4',
+      title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2021, 7, 12),
-      locationOfExpenditure: "Study",
+      date: new Date(2021, 5, 12),
     },
   ];
 
   return (
-    <Card className="App">
-      <h1> Expence Tracker </h1>
-      {expenses.map((detail) => {
-        return (
-          <ExpenseItem
-            title={detail.title}
-            amount={detail.amount}
-            date={detail.date}
-            locationOfExpenditure = {detail.locationOfExpenditure}
-          ></ExpenseItem>
-        );
-      })}
-    </Card>
+    <div>
+      <h2>Expence Tracker</h2>
+      <Expenses item={expenses}/>
+      
+    </div>
   );
 }
 
