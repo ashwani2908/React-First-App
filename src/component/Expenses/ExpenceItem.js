18fa5e21a25bbca40468e586d9607e56d7a7a4b6
ashwani2908 <76448715+ashwani2908@users.noreply.github.com>
2023-04-28 15:10:29 +0530

    Added Event listener to deleteExpense

--- src/component/Expenses/ExpenceItem.js
+++ src/component/Expenses/ExpenceItem.js
@@ -5,11 +5,15 @@ import Card from '../UI/Card';
 
 
 const ExpenseItem = (props) => {
+  const deleteExpense = () => {
+    console.log("Deleted!")
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
         <ExpenseDetail title={props.title} amount={props.amount} locationOfExpenditure={props.locationOfExpenditure}/>
+        <button onClick= {deleteExpense}>Delete Expense</button>
       </div>
     </Card>
   );
